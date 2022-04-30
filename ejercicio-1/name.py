5e19413b6e27b66d251d6c5ae71670d0ee4def1d
Victoriano <mytacam78@gmail.com>
2022-04-30 11:17:41 -0600

    Ejercicio 1

---
+++ ejercicio-1/name.py
@@ -0,0 +1,5 @@
+name = str(input("Ingresa tu nombre: "))
+message1 = 'Estoy empezando el curso de Python'
+message2 = 'Espero aprender mucho'
+
+print(f"Hola, soy {name}.\n {message1}\n {message2}")
